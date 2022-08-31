7a6a0eca92ad781f3e92e4f78bc69e88945c192c
sky4564 <qw486512@gmail.com>
2022-08-31 17:33:45 +0900

    data setting for welcome msg

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
+from email.mime import base
+from re import search
+from tkinter import N
 from requests import get
 from bs4 import BeautifulSoup
 
+
+
 base_url = 'https://weworkremotely.com/remote-jobs/search?term='
-search_item ='python'
+search_item = 'python'
+
+app_info ={
+  'maker' : 'sky4564',
+  'base_url' : base_url,
+  'search_item' : search_item,
+}
+welcome = ('!! welcome to web data scrapper !! \n' \
+            '    maker : ' + app_info['maker'] + '\n'            
+            '    base_url : ' + app_info['base_url'] + '\n'            
+            '    search_item : ' + app_info['search_item'] + '\n')
 
 
 
 response = get(f'{base_url}{search_item}')
 
 if response.status_code == 200:
-  print(f'///////////////// this status_code  : {response.status_code}')
+  print(welcome)
+  print(f' ///////////////// this status_code  : {response.status_code}')
   soup = BeautifulSoup(response.text, "html.parser")
-  paringData = soup.find_all('title')
-  print(paringData)
-else:
-  print(f'this error : {response.status_code}')
-  
+  jobs = soup.find_all('section', class_= 'jobs')
+  for job_section in jobs:
+    job_posts = job_section.find_all('li')
+    for post in job_posts:
+      # print(post)
+      print('////////////////////////////')
+# else:
+  # print(f'this error : {response.status_code}')
