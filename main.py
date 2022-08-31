40b348031563e0be737cb34e23361911ff42db9e
sky4564 <qw486512@gmail.com>
2022-08-31 14:34:35 +0900

    adapt beautifulsoup and testing

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from requests import get
+from bs4 import BeautifulSoup
 
 base_url = 'https://weworkremotely.com/remote-jobs/search?term='
 search_item ='python'
 
+
+
 response = get(f'{base_url}{search_item}')
-print(response)
+
+if response.status_code == 200:
+  print(f'///////////////// this status_code  : {response.status_code}')
+  soup = BeautifulSoup(response.text, "html.parser")
+  paringData = soup.find_all('title')
+  print(paringData)
+else:
+  print(f'this error : {response.status_code}')
+  
