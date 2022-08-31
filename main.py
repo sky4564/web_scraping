d06e76439bd542d8fe2645f22dacc7af3c22a075
sky4564 <qw486512@gmail.com>
2022-08-31 14:14:46 +0900

    testing response from web

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('hello world')
+from requests import get
+
+base_url = 'https://weworkremotely.com/remote-jobs/search?term='
+search_item ='python'
+
+response = get(f'{base_url}{search_item}')
+print(response)
