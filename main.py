9ff740c5321b42e32ee7638a8294b6cf0696e10d
sky4564 <qw486512@gmail.com>
2022-09-13 17:30:37 +0900

    flask html 렌더링 추가 , 최상위 폴더가 templates여야 해서 폴더구조 변경

--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask('job scrapper')
 
 @app.route("/")
 def home():
-    return 'hi there'
-    
+    return "hello world"
+
+@app.route("/hello")
+def hello():
+    return render_template("home.html", name = "kaki")
 
 app.run("0.0.0.0")
-    
-    
-
-
-
-
-
-
-
 
 # from extractors.wwr import extract_wwr_jobs
 # from extractors.file import save_to_file
